65980a7579af7eb21d81ee9361d3063ee972b01c
拔赤 <jay.li@alibaba-inc.com>
2013-05-25 18:01:45 +0800

    commit byjayli

--- pubdoc.sh
+++ pubdoc.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 git pull
-
-git submodule update
-git commit -m "submodules updated"
+git commit -m "tmp commit for pub doc"
 git push
+git submodule update
+git submodule foreach git pull origin master
 
 git checkout gh-pages
 git pull

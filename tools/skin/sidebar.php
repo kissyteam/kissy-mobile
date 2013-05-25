a3a5a902a9d8ce08a76088bb874d59280aa83f00
拔赤 <jay.li@alibaba-inc.com>
2013-05-25 17:48:39 +0800

    comit by jayli bugfix

--- tools/skin/sidebar.php
+++ tools/skin/sidebar.php
@@ -1,20 +1,28 @@
 	  <div class="span2">
+<style>
+#text-4 .textwidget a{
+	display:block;
+}
+
+</style>
 		<div class="widget widget_text" id="text-4">
 			<h4 class="widgettitle">Components</h4>			
 			<div class="textwidget">
-				<a href="markdown.php?mobile/app/1.2/index.md">Mobile App Toolkit</a><br />
-				<a href="markdown.php?mobile/slide/1.1/guide/index.md">Slide</a><br />
-				<a href="http://cubiq.org/iscroll-4">iScroll</a>
+				<a href="markdown.php?mobile/app/1.2/index.md">Mobile App Toolkit</a>
+				<a href="/direct.php?type=doc&name=slide">Slide</a>
 			</div>
 			<h4>Demos</h4>
 			<div class="textwidget">
-				<a href="/mobile/app/1.2/demo/sdk/demo.html">Mobile AppTool kit Demo</a>
-				<a href="/mobile/slide/1.1/demo/">Slide Demo</a>
+				<a href="/mobile/app/1.2/demo/sdk/demo.html"><strong>SDK Demo</strong></a>
+				<a href="/mobile/app/1.2/demo/lottery/demo/app.html">彩票H5</a>
+				<a href="/direct.php?type=demo&name=slide">Slide Demo</a>
 			</div>
 			<h4 class="widgettitle">Links</h4>			
 			<div class="textwidget">
-				<a href="http://docs.kissyui.com">KISSY APIs' Docs</a><br />
-				<a href="http://gallery.kissyui.com">KISSY Gallery</a><br />
+				<a href="http://docs.kissyui.com">KISSY APIs' Docs</a>
+				<a href="http://gallery.kissyui.com">KISSY Gallery</a>
+				<a href="http://cubiq.org/iscroll-4">iScroll</a>
+				<a href="http://pxgrid.github.io/js-flipsnap/start.html">Flipsnap</a>
 			</div>
 		</div>
 	  </div>
